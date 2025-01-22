37dea210d780e45a0114bab86ffe4093a824b783
eliyahugez <eliyahugez@gmail.com>
2025-01-22 13:26:33 +0200

    go

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,16 @@
 
 
 export default function InputLable({ label, isValidInput, ...props }) {
+
+  let labelClassName = "block mb-2 text-xs font-bold tracking-wide uppercase text-stone-300"
+  let inputClassName = "w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow bg-stone-300"
+
+  if (isValidInput) {
+    labelClassName = "block mb-2 text-xs font-bold tracking-wide uppercase text-red-300"
+    inputClassName = "w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow bg-red-300"
+  }
   return <p>
-    <label className="block mb-2 text-xs font-bold tracking-wide uppercase text-stone-300">{label}</label>
-    <input className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow bg-stone-300" {...props} />
+    <label className={labelClassName}>{label}</label>
+    <input className={inputClassName} {...props} />
   </p>
 }
